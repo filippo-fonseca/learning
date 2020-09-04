c9b9828c0ca9a95347ebf7e1c1800bc8c723902c
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-09-03 22:09:12 -0600

    h

---
+++ ts-course/projects/maps/src/CustomMap.ts
@@ -0,0 +1,29 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = this.googleMap = new google.maps.Map(
+      document.getElementById(divId),
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+}
---
+++ ts-course/projects/maps/src/index.ts
@@ -0,0 +1,8 @@
+import { User } from "./User";
+// import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
