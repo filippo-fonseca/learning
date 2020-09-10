24009970156972a8e82f4db848124b50f1961b09
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-09-09 21:39:05 -0600

    h

--- ts-course/projects/sort/src/index.ts
+++ ts-course/projects/sort/src/index.ts
@@ -1,6 +1,7 @@
-import { Sorter } from "./Sorter";
-import { NumbersCollection } from "./NumbersCollection";
+// import { Sorter } from "./Sorter";
+// import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 /* const numbersCollection = new NumbersCollection([100000, 3, -5, 0]);
 
@@ -9,6 +10,14 @@ sorter.sort();
 console.log(numbersCollection.data); */
 
 const charactersCollection = new CharactersCollection("Xaayb");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+
+// linkedList.sort();
+// linkedList.print();
