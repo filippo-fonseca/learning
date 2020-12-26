198e98c6a2025881b2f5e58892bcafac3fcce6b9
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-26 15:25:14 -0600

    feat(programming): Basic C++ CLI

--- programming/cpp/projects/getting-started-proj/src/main.cpp
+++ programming/cpp/projects/getting-started-proj/src/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int favorite_number;
     std::cout
-        << "Enter you favorite number between 1 and 100 > ";
+        << "Enter you favorite number between 1 and 100: ";
     std::cin >> favorite_number;
     std::cout << "Amazing! That's my favorite number, too!" << std::endl;
     return 0;
