60eec7b610fc31265c245cc03ce36c8eccd7355f
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-31 17:07:41 -0600

    reft[programming/gql] => 🧠 Add loading screen

--- programming/graphql/projects/todo-graphql/src/App.js
+++ programming/graphql/projects/todo-graphql/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_TODOS } from "./graphql/queries";
+import { CircularProgress } from "@material-ui/core";
 
 function App() {
   const { data, loading, error } = useQuery(GET_TODOS);
 
-  if (loading) return <div>Loading...</div>
+  if (loading) return <div style={{ display: "flex", flexDirection: "column", alignItems: "center"}}><CircularProgress /></div>
 
   if (error) return <div>Something went wrong...</div>
   return (
